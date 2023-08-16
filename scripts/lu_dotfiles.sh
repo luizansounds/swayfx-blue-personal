777fcd70759868675d9b6a5377816d05c90cac31
Luiza <114603943+Luiza1011@users.noreply.github.com>
2023-08-16 18:11:30 -0300

    Update lu_dotfiles.sh

--- scripts/lu_dotfiles.sh
+++ scripts/lu_dotfiles.sh
@@ -1,1 +1,19 @@
+#!/usr/bin/env sh
+#commit
+git clone -C ~/.local/ https://github.com/Luiza1011/lu-dotfiles
 
+ln -r -s ~/.local/lu-dotfiles/.config/sway/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/swaync/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/waybar/ ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.config/rofi / ~/.config/
+
+ln -r -s ~/.local/lu-dotfiles/.zshrc ~/
+
+ln -r -s ~/.local/lu-dotfiles/.fonts/ ~/
+
+ln -r -s ~/.local/lu-dotfiles/.themes/ ~/
+
+ln -r -s ~/.local/lu-dotfiles/.icons/ ~/
