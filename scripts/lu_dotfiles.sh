55a13bcce5413eafbfee125d1c471b3ab3f9ccca
Luiza <114603943+Luiza1011@users.noreply.github.com>
2023-08-16 18:32:09 -0300

    Update lu_dotfiles.sh

--- scripts/lu_dotfiles.sh
+++ scripts/lu_dotfiles.sh
@@ -5,20 +5,20 @@ toolbox create --image quay.io/toolbx-images/opensuse-toolbox:tumbleweed suse-bo
 toolbox create --image quay.io/toolbx-images/archlinux-toolbox:latest arch-box
 
 #commit
-git clone -C ~/.local/ https://github.com/Luiza1011/lu-dotfiles
+git clone  https://github.com/Luiza1011/lu-dotfiles /var/home/luiza/.local/
 
-ln -r -s ~/.local/lu-dotfiles/.config/sway/ ~/.config/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.config/sway/ ~/.config/
 
-ln -r -s ~/.local/lu-dotfiles/.config/swaync/ ~/.config/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.config/swaync/ ~/.config/
 
-ln -r -s ~/.local/lu-dotfiles/.config/waybar/ ~/.config/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.config/waybar/ ~/.config/
 
-ln -r -s ~/.local/lu-dotfiles/.config/rofi / ~/.config/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.config/rofi / ~/.config/
 
-ln -r -s ~/.local/lu-dotfiles/.zshrc ~/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.zshrc /var/home/
 
-ln -r -s ~/.local/lu-dotfiles/.fonts/ ~/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.fonts/ /var/home/
 
-ln -r -s ~/.local/lu-dotfiles/.themes/ ~/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.themes/ /var/home/
 
-ln -r -s ~/.local/lu-dotfiles/.icons/ ~/
+ln -r -s /var/home/luiza/.local/lu-dotfiles/.icons/ /var/home/
